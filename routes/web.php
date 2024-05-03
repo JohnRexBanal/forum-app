ab7f000f7c47ae1f7d30ab049cd6a8f6fd5625a9
rinoaxyrl <almodovarrinoaxyrel73@gmail.com>
2024-05-03 19:32:08 +0800

    defined a route for the registration form

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,8 @@ Route::get('show/{id}', [PostController::class, 'show'])->name('show');
 Route::get('edit/{id}', [PostController::class, 'edit'])->name('edit');
 Route::put('update/{id}', [PostController::class, 'update'])->name('update');
 Route::delete('delete/{id}', [PostController::class, 'destroy'])->name('delete');
+Route::get('/register', function () {
+    return view('register');
+});
 
 
