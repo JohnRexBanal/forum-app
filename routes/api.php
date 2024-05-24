a36b264ba9a375f31e597195f13f0076d7679f0d
JohnRexBanal <banaljohnrex99@gmail.com>
2024-05-24 14:35:26 +0800

    Modified files - added api on guards in auth.php - added url's for vue and laravell in allowed_origins - modified the api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+
+    Route::post('/login', [AuthenticationController::class, 'login']);
+    Route::post('/register', [AuthenticationController::class, 'register']);
+    Route::post('/logout', [AuthenticationController::class, 'logout'])->middleware('auth:sanctum');
+
+
+   
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('/posts', [PostController::class, 'index']);
+        Route::post('/create', [PostController::class, 'store']);
+        Route::get('/edit/{id}', [PostController::class, 'edit']);
+        Route::put('/update/{id}', [PostController::class, 'update']);
+        Route::delete('/delete/{id}', [PostController::class, 'destroy']);
+        Route::get('/view/{id}', [PostController::class, 'show']);
+    });
